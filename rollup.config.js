a1365719533ada3b56efb7a7e8bc54c07d981456
Marcel Hoekstra <marcel.hoekstra@cito.nl>
2021-03-09 22:17:57 +0100

    publish script

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,8 @@ import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import typescript from "rollup-plugin-typescript2";
-
-import packageJson from "./package.json";
+import copy from "rollup-plugin-copy";
+import packageJson from "./libs/rx-basic-store/package.json";
 
 export default {
   input: "./libs/rx-basic-store/src/index.ts",
@@ -19,5 +19,8 @@ export default {
       sourcemap: true
     }
   ],
-  plugins: [peerDepsExternal(), resolve(), commonjs(), typescript()]
+  plugins: [peerDepsExternal(), resolve(), commonjs(), typescript(),
+    copy({
+      targets: [{ src: ['./libs/rx-basic-store/package.json', './README.md'], dest: 'dist' }]
+    })]
 };
