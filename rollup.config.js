0b2c13c002c04b5d0d34ec07c036ed7219a6a0bb
Marcel Hoekstra <marcel.hoekstra@cito.nl>
2021-03-10 13:13:45 +0100

    added types

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,33 @@
-import commonjs from "@rollup/plugin-commonjs";
-import resolve from "@rollup/plugin-node-resolve";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import typescript from "rollup-plugin-typescript2";
-import copy from "rollup-plugin-copy";
-import packageJson from "./package.json";
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import typescript from 'rollup-plugin-typescript2';
+import copy from 'rollup-plugin-copy';
+import packageJson from './package.json';
 
 export default {
-  input: "./libs/rx-basic-store/src/index.ts",
+  input: './libs/rx-basic-store/src/index.ts',
   output: [
     {
       file: packageJson.main,
-      format: "cjs",
-      sourcemap: true
+      format: 'cjs',
+      sourcemap: true,
     },
     {
       file: packageJson.module,
-      format: "esm",
-      sourcemap: true
-    }
+      format: 'esm',
+      sourcemap: true,
+    },
   ],
-  plugins: [peerDepsExternal(), resolve(), commonjs(), typescript(),
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript({
+      tsconfig: './libs/rx-basic-store/tsconfig.lib.json'
+    }),
     copy({
-      targets: [{ src: ['./package.json', './README.md'], dest: 'dist/lib' }]
-    })]
+      targets: [{ src: ['./package.json', './README.md'], dest: 'dist/lib' }],
+    }),
+  ],
 };
